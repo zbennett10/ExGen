04513acb1426db269ca2d319cb6449c87231e6e0
zbennett10 <zbennett10@Hotmail.com>
2016-12-20 21:11:04 -0500

    index ui start

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Login/Register</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        
+        <style>
+            body {
+                background-color: cadetblue;
+            }
+
+            .container {
+                background-color: goldenrod;
+                width: 50em;
+                height: 20em;
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                margin: auto;
+                text-align: center;
+
+            }
+
+            a {
+                text-decoration: none;
+                color: white;
+                font-size: 36px;
+                font-family: 'montserrat';
+            }
+
+        </style>
     </head>
 
     <body>
-        <?php
-            echo "<p>Hello World</p>";
-
-        ?>
-        <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
+        <div class="container">
+            <a href="login.php">Login</a> </br>
+            <a href="register.php">Register</a>
+        </div>
     </body>
 </html
